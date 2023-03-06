8021e48eb60ad84d346aa0bf50a09cc3e0e8598f
rxin <rxin@connect.ust.hk>
2023-03-06 12:38:43 +0000

    feat: update EularPre to be compatible with DDP, no pretrain is acceptable

--- common_utils.py
+++ common_utils.py
@@ -7,14 +7,14 @@ FilePath: /DIPP/common_utils.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import torch
-from model.predictor import Predictor, RiskMapPre
+from model.predictor import EularPre, Predictor, RiskMapPre
 from model.planner import BasePlanner, EularSamplingPlanner, MotionPlanner, RiskMapPlanner
 from utils.riskmap.car import pi_2_pi
 from utils.train_utils import bicycle_model, select_future
 predictor_selection = {'base': Predictor,
                        'dipp': Predictor,
                        'risk': RiskMapPre,
-                       'esp': RiskMapPre,
+                       'esp': EularPre,
                        }
 
 
